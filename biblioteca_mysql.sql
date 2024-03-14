9d3e6f6635ae771633c3a9389e39f0e3f42cf497
Gabriel Cortes <118791553+GabrielRyuu@users.noreply.github.com>
2024-03-14 17:34:55 -0300

    Update biblioteca_mysql.sql

---
+++ biblioteca_mysql.sql
@@ -0,0 +1,24 @@
+CREATE TABLE livros (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(255) NOT NULL,
+    isbn VARCHAR(20),
+    ano_publicacao INTEGER,
+    editora VARCHAR(100),
+    estoque INT
+);
+
+CREATE TABLE emprestimos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    livro_id INTEGER,
+    data_emprestimo DATE,
+    data_devolucao DATE,
+    pessoa_emprestimo VARCHAR(255),
+    status VARCHAR(20),
+    FOREIGN KEY (livro_id) REFERENCES livros(id)
+);
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL
+);
